f566799ef3c2e99202897154e1153ef69d5fcdf9
[harshit gakhar] <harshitga@gmail.com>
2020-11-29 02:27:51 +0530

    update porfolio

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-
+import './../Style/Main.css'
+import Books from './../Asset/001-documents.png'
+import File from './../Asset/029-light bulb.png'
+import Home from './../Asset/005-id card.png'
 
 export const Navbar = () => {
+    const [home,useHome] = useState(false);
+      const [skills,useSkills] = useState(false);
+      const [projects,useProjects] = useState(false);
+      const [contact,useContact] = useState(false);
+
+
   return (
-    <div>
-      hello
-    </div>
+      <div className="nav">
+                <div className="imageS" style={{marginTop:100}} onMouseEnter={(e)=>console.log(e.target.textContent)}>
+                  {home ? ('') : ('Home')}
+                </div>
+                <div className="imageS">
+                  {skills ? ('') : ('Skills')}
+                </div>
+                <div className="imageS">
+                  {projects ? ('') : ('Projects')}
+                </div>
+                <div className="imageS">
+                  {contact ? ('') : ('Contact')}
+                </div>
+        </div>
   )
 }
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as RouterMain, Link,Route } from 'react-router-dom'
+import Contact from '../Views/Contact';
 import Main from '../Views/Main';
+import Project from '../Views/Project';
+import Skills from '../Views/Skills';
 
 function Locator() {
   return (
@@ -10,6 +13,9 @@ function Locator() {
       </React.Fragment>
       <React.Fragment>
         <Route path="/" exact component={Main} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/project" exact component={Project} />
+        <Route path="/skill" exact component={Skills} />
       </React.Fragment>
     </RouterMain>
   )
